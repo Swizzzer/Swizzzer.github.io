2f9afc0546028c2900ee91e14011e9cf1bc4aa58
Swizzer <kyoshaft@outlook.com>
2025-06-10 01:46:21 +0800

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://eupho.me/",
-	base: "Swizzzer/blog",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
